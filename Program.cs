3345f7d49b2e0776778a32eff64b75a49c6a79da
Sairaj-Tamboskar-Kilowott <92842383+Sairaj-Tamboskar-Kilowott@users.noreply.github.com>
2021-12-05 15:21:26 +0530

    commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+
+namespace emi_calc
+{
+
+public class FirstCalculator
+  {
+
+    // funtion to calculate EMI (formula)
+    public double emi_calculator(double p, double i, double t, double n)
+    {
+        i = i / 100;
+        double emi = (p * Math.Pow((1 + i / n), (t * n)))/24;
+
+        return (emi);
+    }
+
+    // main class
+    public static void Main(String[] args)
+    {
+        //object created
+        var take = new FirstCalculator();
+
+        //taking user input
+        Console.WriteLine("EMI Calculator for 2 wheeler"); 
+        Console.WriteLine("Enter Principle Amount:");          
+        double p = Convert.ToDouble(Console.ReadLine());   
+        Console.WriteLine("Rate of Interest:");                
+        double i = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter number of years:");             
+        double t = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Monthly Compounding:");             
+        double n = Convert.ToDouble(Console.ReadLine());
+        double
+
+        //calling the function
+        emi = take.emi_calculator(p, i, t, n);
+
+        // print output
+        Console.WriteLine("Monthly EMI is:" + emi);
+
+
+    }
+  }
+
+}
